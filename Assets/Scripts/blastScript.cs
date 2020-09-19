7023178a0c056b94bf866cac4a234e1599843258
M-Shahbaz-Khan <shahbazkhan2794@gmail.com>
2020-09-19 10:23:49 +0500

    fix for blast trigger showing under the road
    disable its mesh renderer when not in use

--- Assets/Scripts/blastScript.cs
+++ Assets/Scripts/blastScript.cs
@@ -20,6 +20,9 @@ public class blastScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // turn off mesh renderer to make invisible when not in use
+        GetComponent<MeshRenderer>().enabled = false;
+
         rb.position = new Vector3(0, -40, -15);
         minScale = new Vector3(1.5f, 1, 1);
         maxScale = new Vector3(3, 1, 1);
@@ -35,13 +38,15 @@ public class blastScript : MonoBehaviour
     {
         if (blastOn == true)
         {
+
             float currentRange = rb.transform.position.z - startPosZ;
             if (currentRange > maxRange)
             {
-               
+
                 rb.position = new Vector3(0, -40, 15);
                 rb.transform.localScale = minScale;
                 rb.velocity = Vector3.zero;
+                GetComponent<MeshRenderer>().enabled = false;
                 blastOn = false;
             }
             else
@@ -59,6 +64,7 @@ public class blastScript : MonoBehaviour
             {
                 if (blastRecharge < 0)
                 {
+                    GetComponent<MeshRenderer>().enabled = true;
                     Vector3 playerPos = playerRb.transform.position;
                     startPosZ = playerPos.z;
                     Vector3 playerVel = playerRb.velocity;
@@ -67,7 +73,7 @@ public class blastScript : MonoBehaviour
                     blastOn = true;
                 }
             }
-            
+
         }
     }
 
