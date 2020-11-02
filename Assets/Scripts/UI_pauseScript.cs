d6a8f312e110d5d4e264be54edc53d238bc8617f
M-Shahbaz-Khan <shahbazkhan2794@gmail.com>
2020-11-02 19:20:09 +0500

    Begin implementing pause menu

---
+++ Assets/Scripts/UI_pauseScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_pauseScript : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    void Update(){
+      if(Input.GetKeyDown(KeyCode.Escape))
+        clickPauseButton();
+    }
+
+    // Start is called before the first frame update
+    public void clickPauseButton()
+    {
+        if (isPaused){
+            Debug.Log("Unpaused.");
+            resume();
+        }
+        else
+        {
+            Debug.Log("Paused.");
+            pause();
+        }
+
+        return;
+    }
+
+    private void resume(){
+      isPaused = false;
+      return;
+    }
+
+    private void pause(){
+      isPaused = true;
+      return;
+    }
+}
