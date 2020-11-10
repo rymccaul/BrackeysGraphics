a27bf3acfdeede847bb396ddcbef15618985d436
Shahbaz Khan <shahbazkhan2794@gmail.com>
2020-11-11 02:12:05 +0500

    Added FPS to UI

---
+++ Assets/Scripts/UI_FPSScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UI_FPSScript : MonoBehaviour
+{
+  public float updateInterval = 0.5F;
+  private double lastInterval;
+  private int frames = 0;
+  private float fps;
+  private TextMeshProUGUI FPS_text_obj;
+
+  void Start()
+  {
+      FPS_text_obj = GameObject.Find("UI_FPS").GetComponent<TMPro.TextMeshProUGUI>();
+      lastInterval = Time.realtimeSinceStartup;
+      frames = 0;
+  }
+
+  void Update()
+  {
+      ++frames;
+      float timeNow = Time.realtimeSinceStartup;
+      if (timeNow > lastInterval + updateInterval)
+      {
+          fps = (float)(frames / (timeNow - lastInterval));
+          Debug.Log((timeNow - lastInterval).ToString() + ", " + frames.ToString());
+          frames = 0;
+          lastInterval = timeNow;
+          FPS_text_obj.text = "FPS | " + fps.ToString();
+      }
+
+
+  }
+}
