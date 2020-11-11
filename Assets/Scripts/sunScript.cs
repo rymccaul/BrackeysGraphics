8c59d19801c0b92c4cfc7d8292c84684f12729ca
rymccaul <rymccaul@users.noreply.github.com>
2020-11-11 13:09:02 -0800

    sun shader done

---
+++ Assets/Scripts/sunScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunScript : MonoBehaviour
+{
+    private Vector3 offsetFromPlayer;
+    public Transform cameraTransform;
+    private Vector3 sunPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offsetFromPlayer = new Vector3(0, 39.95f, 593.6f);
+        transform.position = offsetFromPlayer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraTransform.position + offsetFromPlayer;
+    }
+}
