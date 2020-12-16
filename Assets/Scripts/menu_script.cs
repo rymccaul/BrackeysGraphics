fdf46123f93f012ee7d9dea0ef68e57a6121492b
Shahbaz Khan <shahbazkhan2794@gmail.com>
2020-12-17 02:38:52 +0500

    Finished basic main menu

---
+++ Assets/Scripts/menu_script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class menu_script : MonoBehaviour
+{
+    public GameObject MainMenu;
+    public GameObject SettingsMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainMenuButton();
+    }
+
+    public void PlayNowButton()
+    {
+        // Play Now Button has been pressed, here you can initialize your game (For example Load a Scene called GameLevel etc.)
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+
+    public void SettingsButton()
+    {
+        // Show Credits Menu
+        MainMenu.SetActive(false);
+        SettingsMenu.SetActive(true);
+    }
+
+    public void MainMenuButton()
+    {
+        // Show Main Menu
+        MainMenu.SetActive(true);
+        SettingsMenu.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        // Quit Game
+        Application.Quit();
+    }
+}
